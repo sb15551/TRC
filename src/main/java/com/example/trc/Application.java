41991e2d6bded9a87af8968ac353fec95ef8565a
Alexey Surkov <stibium128@gmail.com>
2020-11-30 20:38:58 +0400

    Add maven project

---
+++ src/main/java/com/example/trc/Application.java
@@ -0,0 +1,15 @@
+package com.example.trc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Surkov Aleksey (stibium128@gmail.com)
+ * @date 07.10.2020 13:06
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
